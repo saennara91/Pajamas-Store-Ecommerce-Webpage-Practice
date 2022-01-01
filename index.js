2eae68cfcecdf24d49e4f3973d4ce77a6e703571
Saennara <saennara@Saennaras-MacBook-Pro.local>
2022-01-01 12:48:03 +0700

    Edit navbar and font color

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const hamburger = document.querySelector('.hamburger');
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
-hamburger.addEventListener('click', () => {
-    this.classList.toggle('active');
-})
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
+}
